9e6d84fa7c2e96d11dd75b4e5469b16509dac28c
Uday Shetty <udayshetty@Udays-MacBook-Air.local>
2021-03-28 12:10:37 +0530

    init commit

---
+++ packages/container/src/components/marketinApp.js
@@ -0,0 +1,23 @@
+//this function allows us to render from our child application 
+//this function will work with any child framework angulat,vue etc 
+//As long as output is html this function will work
+
+import { mount } from 'marketing/MarketingApp'
+
+import React, { useRef, useEffect } from 'react'
+
+
+const marketinApp = () => {
+
+  useEffect(() => {
+    mount(ref.current)
+  })
+  const ref = useRef(null)
+  return (
+    <div ref={ref}>
+
+    </div>
+  )
+}
+
+export default marketinApp
