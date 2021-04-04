64fc6b65e6a082cd396ec28b67dcabe823534fe9
swagath shetty <udayshetty@Udays-MacBook-Air.local>
2021-04-04 11:38:52 +0530

    fixed the path issue for production

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',//done for caching issues
+    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
