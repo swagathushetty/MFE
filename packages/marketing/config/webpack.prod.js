6bd26c409e5966adf2dafe0728f0c8cebb715b5e
swagath shetty <udayshetty@Udays-MacBook-Air.local>
2021-04-05 13:06:18 +0530

    fixed typo for webpack config

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -13,7 +13,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
-      filename: 'remoteEntry.js'
+      filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap'
       },
