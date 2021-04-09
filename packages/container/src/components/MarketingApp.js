2d0606de70a80a4eea33613ec76a4b99ec9a3814
swagath shetty <udayshetty@Udays-MacBook-Air.local>
2021-04-09 18:59:26 +0530

    added auth app to S3

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -8,6 +8,7 @@ import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'
 
 export default () => {
+
   const ref = useRef(null);
   const history = useHistory()
 
