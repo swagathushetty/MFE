d324684ff0a7923928b621f3d36e08fd3e73edad
swagath shetty <udayshetty@Udays-MacBook-Air.local>
2021-04-15 17:37:37 +0530

    added dashboard app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,28 @@
-import React, { lazy, Suspense, useState } from 'react'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import Header from './components/Header'
 import Progress from './components/Progress'
-
+import { createBrowserHistory } from 'history'
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashBoardLazy = lazy(() => import('./components/DashBoardApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const history = createBrowserHistory()
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
@@ -23,12 +31,17 @@ export default () => {
               <Route path="/auth"  >
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashBoardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
+
+
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
---
+++ packages/container/src/components/DashBoardApp.js
@@ -0,0 +1,18 @@
+//this function allows us to render from our child application 
+//this function will work with any child framework angulat,vue etc 
+//As long as output is html this function will work
+
+
+import { mount } from 'dashboard/DashboardApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+
+  }, []);
+
+  return <div ref={ref} />;
+};
